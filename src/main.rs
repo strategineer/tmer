907a95f1ca1e7203633e1a5896660afe1d283c78
strategineer <me@strategineer.com>
2020-09-17 09:39:30 -0400

    Implement structs to better manage data instead of just outputting it

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,156 @@
 extern crate clap;
-use log::{info,Level};
+use clap::{App, Arg};
+use log::{info, Level};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use std::fmt;
 use std::fs::File;
 use std::io::{self, BufRead};
-use rand::thread_rng;
-use rand::seq::SliceRandom;
-use clap::{Arg, App};
 use std::path::Path;
+
+#[derive(Clone, Debug)]
+struct Team {
+    members: Vec<String>,
+}
+
+impl Team {
+    pub fn new() -> Team {
+        Team {
+            members: Vec::new(),
+        }
+    }
+    pub fn add_member(&mut self, m: String) {
+        self.members.push(m);
+    }
+}
+
+impl fmt::Display for Team {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.members.join(","))
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Round {
+    teams: Vec<Team>,
+}
+
+impl Round {
+    pub fn new() -> Round {
+        Round { teams: Vec::new() }
+    }
+    pub fn add_team(&mut self, t: Team) {
+        self.teams.push(t);
+    }
+}
+
+impl fmt::Display for Round {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.teams.len() {
+            0 => write!(f, ""),
+            _ => write!(
+                f,
+                "{}",
+                self.teams
+                    .clone()
+                    .into_iter()
+                    .map(|t| t.to_string())
+                    .collect::<Vec<String>>()
+                    .join("\n")
+            ),
+        }
+    }
+}
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-fn generate_teams(n_players: usize, n_teams: usize, team_size: usize, ids: Vec<String>) {
+fn generate_teams(n_players: usize, n_teams: usize, team_size: usize, ids: &[String]) -> Round {
+    let mut round: Round = Round::new();
     let mut start_index: usize;
     let mut end_index: usize = 0;
     for t in 0..n_teams {
+        let mut team: Team = Team::new();
         start_index = t * team_size;
         end_index = t * team_size + team_size;
         info!("{}:{}", start_index, end_index);
         for i in start_index..end_index {
-            print!("{} ", ids.get(i).unwrap().to_string());
+            team.add_member(ids.get(i).unwrap().to_string());
         }
-        println!()
+        round.add_team(team);
     }
     if end_index <= n_players {
+        let mut team: Team = Team::new();
         for i in end_index..n_players {
-            print!("{} ", ids.get(i).unwrap().to_string());
+            team.add_member(ids.get(i).unwrap().to_string());
         }
+        round.add_team(team);
     }
+    return round;
 }
-
 
 fn run_app() -> Result<(), ()> {
     let matches = App::new("Tmer")
         .version("1.0")
         .author("strategineer <me@strategineer.com>")
         .about("Make teams")
-        .arg(Arg::with_name("file")
-            .help("...")
-            .short("f")
-            .long("file")
-            .value_name("FILEPATH")
-            .conflicts_with("n_players")
-            .takes_value(true))
-        .arg(Arg::with_name("n_players")
-            .help("...")
-            .short("n")
-            .long("count")
-            .value_name("NUMBER_OF_PLAYERS")
-            .conflicts_with("file")
-            .takes_value(true))
-        .arg(Arg::with_name("n_teams")
-            .help("...")
-            .short("t")
-            .long("teams")
-            .conflicts_with("n_size")
-            .value_name("NUMBER_OF_TEAMS")
-            .takes_value(true))
-        .arg(Arg::with_name("n_size")
-            .help("...")
-            .short("s")
-            .long("size")
-            .conflicts_with("n_teams")
-            .value_name("TEAM_SIZE")
-            .takes_value(true))
-        .arg(Arg::with_name("n_rounds")
-            .help("...")
-            .short("r")
-            .long("rounds")
-            .value_name("NUMBER_OF_ROUNDS")
-            .takes_value(true))
-        .arg(Arg::with_name("debug")
-            .help("print debug information verbosely")
-            .short("d")
-            .long("debug"))
+        .arg(
+            Arg::with_name("file")
+                .help("...")
+                .short("f")
+                .long("file")
+                .value_name("FILEPATH")
+                .conflicts_with("n_players")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("n_players")
+                .help("...")
+                .short("n")
+                .long("count")
+                .value_name("NUMBER_OF_PLAYERS")
+                .conflicts_with("file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("n_teams")
+                .help("...")
+                .short("t")
+                .long("teams")
+                .conflicts_with("n_size")
+                .value_name("NUMBER_OF_TEAMS")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("n_size")
+                .help("...")
+                .short("s")
+                .long("size")
+                .conflicts_with("n_teams")
+                .value_name("TEAM_SIZE")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("n_rounds")
+                .help("...")
+                .short("r")
+                .long("rounds")
+                .value_name("NUMBER_OF_ROUNDS")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("debug")
+                .help("print debug information verbosely")
+                .short("d")
+                .long("debug"),
+        )
         .get_matches();
     let is_debug = matches.is_present("debug");
 
@@ -92,21 +165,20 @@ fn run_app() -> Result<(), ()> {
     let n_rounds_str = matches.value_of("n_rounds");
     let n_rounds: usize;
     match n_rounds_str {
-        None => panic!("Either the -t or -s parameters must be set"),
-        Some(s) => {
-            match s.parse::<usize>() {
-                Ok(n) => {
-                    n_teams = n_players / n;
-                    team_size = n;
-                },
-                Err(err) => panic!("Size parameter must be a number: {:?}", err)
-            }
+        None => {
+            n_rounds = 1;
         }
+        Some(s) => match s.parse::<usize>() {
+            Ok(n) => {
+                n_rounds = n;
+            }
+            Err(err) => panic!("Size parameter must be a number: {:?}", err),
+        },
     }
 
     let n_players_str = matches.value_of("n_players");
     let n_players: usize;
-    let mut ids : Vec<String>;
+    let mut ids: Vec<String>;
     match n_players_str {
         None => {
             let file_str = matches.value_of("file");
@@ -134,15 +206,13 @@ fn run_app() -> Result<(), ()> {
                 }
             }
         }
-        Some(s) => {
-            match s.parse::<usize>() {
-                Ok(n) => {
-                    n_players = n;
-                    ids = (1..n_players + 1).map(|x| x.to_string()).collect();
-                },
-                Err(err) => panic!("n_players parameter must be a number: {:?}", err)
+        Some(s) => match s.parse::<usize>() {
+            Ok(n) => {
+                n_players = n;
+                ids = (1..n_players + 1).map(|x| x.to_string()).collect();
             }
-        }
+            Err(err) => panic!("n_players parameter must be a number: {:?}", err),
+        },
     }
     let n_teams: usize;
     let team_size: usize;
@@ -152,40 +222,39 @@ fn run_app() -> Result<(), ()> {
             let team_size_str = matches.value_of("n_size");
             match team_size_str {
                 None => panic!("Either the -t or -s parameters must be set"),
-                Some(s) => {
-                    match s.parse::<usize>() {
-                        Ok(n) => {
-                            n_teams = n_players / n;
-                            team_size = n;
-                        },
-                        Err(err) => panic!("Size parameter must be a number: {:?}", err)
+                Some(s) => match s.parse::<usize>() {
+                    Ok(n) => {
+                        n_teams = n_players / n;
+                        team_size = n;
                     }
-                }
-            }
-        }
-        Some(s) => {
-            match s.parse::<usize>() {
-                Ok(n) => {
-                    n_teams = n;
-                    team_size = n_players / n;
+                    Err(err) => panic!("Size parameter must be a number: {:?}", err),
                 },
-                Err(err) => panic!("Team parameter must be a number: {:?}", err)
             }
         }
+        Some(s) => match s.parse::<usize>() {
+            Ok(n) => {
+                n_teams = n;
+                team_size = n_players / n;
+            }
+            Err(err) => panic!("Team parameter must be a number: {:?}", err),
+        },
     }
 
     info!("n_players: {}", n_players);
     info!("n_teams: {}", n_teams);
     info!("team_size: {}", team_size);
     info!("ids: {:?}", ids);
-    ids.shuffle(&mut thread_rng());
-    info!("shuffled: {:?}", ids);
 
     if n_players < team_size {
         panic!("Team size must be smaller than the number of players")
     }
 
-    generate_teams(n_players, n_teams, team_size, ids);
+    for _ in 0..n_rounds {
+        ids.shuffle(&mut thread_rng());
+        info!("shuffled: {:?}", ids);
+        let round = generate_teams(n_players, n_teams, team_size, &ids);
+        println!("{}", round);
+    }
 
     Ok(())
 }
